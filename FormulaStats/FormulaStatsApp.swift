de6223a0dfa50b08900f68ca20f3b2cf74f632a8
JacobSeiberg <jakeseiberg@gmail.com>
2024-10-22 12:34:00 -0700

    Initial Commit

---
+++ FormulaStats/FormulaStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaStatsApp.swift
+//  FormulaStats
+//
+//  Created by Jacob Seiberg on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FormulaStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
